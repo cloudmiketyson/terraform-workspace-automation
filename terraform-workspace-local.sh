5d2d49b28d835cc6742f28f99191978e93029a3d
cloudmiketyson <134102900+cloudmiketyson@users.noreply.github.com>
2023-08-27 16:03:30 -0400

    Update terraform-workspace-local.sh

--- terraform-workspace-local.sh
+++ terraform-workspace-local.sh
@@ -6,14 +6,6 @@
 # 3) create workspace if it does not exist
 ##########################################################
 set -e
-file_locations=$(find . -maxdepth 2 -type f \( -name "backend.tf" \))
-
-for f in $file_locations
-do
-    TF_MODULE_PATH=$(dirname $f) bash -c \
-        "$(curl -fsSL https://raw.githubusercontent.com/cloudmiketyson/miscellanious/main/tools/terraform-create-all-workspaces.sh"
-done
-
 organization=${TF_ORGANIZATION:="thiesco-DevOps"}
 terraformapiurl=${TF_API_URL:="https://app.terraform.io/api/v2"}
 terraformversion=${TF_VERSION:=1.0.0}
